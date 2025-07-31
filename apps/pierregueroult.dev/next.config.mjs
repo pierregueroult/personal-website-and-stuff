b21ba3b6a91ed86f2a164302b40f65ca7b51ac44
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-07-31 11:48:06 +0200

    feat: update nextjs and turn on react compiler

--- apps/pierregueroult.dev/next.config.mjs
+++ apps/pierregueroult.dev/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@repo/ui'],
+  experimental: {
+    reactCompiler: true,
+    typedRoutes: true,
+  },
+  poweredByHeader: false,
 };
 
 export default nextConfig;
